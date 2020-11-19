b7b77e442360c63d16e8fd770d2d63beb56f7b05
Ben Spotswood <spotswoodb@gmail.com>
2020-11-19 10:24:01 -0600

    commit tree 69c59182c2ab30f816f629e7388903b4170ffe24

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,48 @@
+number = []
+foods = []
+def sort_array_asc(number)
+    number.sort do |a, b|
+        a <=> b
+    end
+end
 
+def sort_array_desc(number)
+    number.sort.reverse do |a, b|
+        a <=> b
+    end
+end
+
+def sort_array_char_count(foods)
+    foods.sort_by {|x| x.length}
+end
+
+def swap_elements(array)
+    array[1], array[2] = array[2], array[1]
+    array
+end
+
+def reverse_array(array)
+    array.reverse
+end
+
+def kesha_maker(array)
+    array.each {|letter| letter[2] = "$"}
+end
+
+def find_a(array)
+    array.select {|element| element.start_with?("a")}
+end
+
+def sum_array(array)
+    array.inject {|sum, n| sum + n}
+end
+
+def add_s(array)
+    array.each_with_index.collect{|element, index|  
+        if index == 1
+        element = element
+        else
+        element = element + "s"
+    end
+    }
+end
